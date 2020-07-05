620dae9d644e600f044273edcb389e8842fcb768
Fred Silveira <fredericos@eu.square-enix.com>
2020-07-05 01:45:10 +0100

    created shop page with shop data and collectionPreview component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import './App.css';
-import HomePage from "./pages/homepage/homepage";
-import "./pages/homepage/homepage.scss";
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import './App.css';
+
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ HomePage }/>
-        <Route path="/hats" component={ HatsPage }/>
+        <Route path="/shop" component={ ShopPage }/>
       </Switch>
     </div>
   );
