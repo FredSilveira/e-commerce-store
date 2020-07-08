5039a124ba2fac4227799a7224aa3db7af291cb9
Fred Silveira <fredericos@eu.square-enix.com>
2020-07-08 23:13:42 +0100

    added sign in / sign up page and components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from "react-router-dom";
 
 import './App.css';
 
+import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
-import Header from "./components/header/header";
+import SignInSignUpPage from "./pages/sign-in-sign-up/signInSignUp";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ HomePage }/>
         <Route path="/shop" component={ ShopPage }/>
+        <Route path="/signin" component={ SignInSignUpPage }/>
       </Switch>
     </div>
   );
