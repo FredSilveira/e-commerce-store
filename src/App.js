14711575aed28dfc1bb03141744202de6e09fa79
Fred Silveira <fredericos@eu.square-enix.com>
2020-07-15 23:57:37 +0100

    user authentication tracker and unsubscribe from auth

--- src/App.js
+++ src/App.js
@@ -7,18 +7,43 @@ import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import SignInSignUpPage from "./pages/sign-in-sign-up/signInSignUp";
+import { auth } from "../src/firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ HomePage }/>
-        <Route path="/shop" component={ ShopPage }/>
-        <Route path="/signin" component={ SignInSignUpPage }/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ current: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ HomePage }/>
+          <Route path="/shop" component={ ShopPage }/>
+          <Route path="/signin" component={ SignInSignUpPage }/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
